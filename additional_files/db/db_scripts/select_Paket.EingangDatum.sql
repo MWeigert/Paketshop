6f1dc6b4eec15a27ef4770c21c0183e0b470b65d
Miro Ljubicic <miro@ljubicic.ch>
2011-11-30 13:23:54 +0100

    Einige SQL-Scripts erstellt

---
+++ additional_files/db/db_scripts/select_Paket.EingangDatum.sql
@@ -0,0 +1,13 @@
+----------------------------------------------
+-- Fallstudie "Paketshop"
+-- Kurs:    Methoden der Programmierung
+-- Author:  Miro Ljubicic
+-- Version: 0.1
+-- Date:    29.11.2011
+-- Zweck:   Auslesen des Paket-Eingangsdatums
+----------------------------------------------
+
+SELECT
+`Paket`.`EingangDatum`
+FROM `paketshop`.`Paket`
+WHERE `Paket`.`Id` = ???;
