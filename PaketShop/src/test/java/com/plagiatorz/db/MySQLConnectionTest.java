a0b39b87cba73b7a2aa310e841b8be5698a38c06
Marius Mueller <mmuelle5@hsz-t.ch>
2011-12-01 18:50:31 +0100

    Testklassen und Maven Build

---
+++ PaketShop/src/test/java/com/plagiatorz/db/MySQLConnectionTest.java
@@ -0,0 +1,29 @@
+package src.test.java.com.plagiatorz.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+
+public class MySQLConnectionTest extends TestCase {
+
+	public void testConnection() {
+		String dbUrl = "jdbc:mysql://www.fun-diver.ch:3306/paketshop";
+		String dbClass = "com.mysql.jdbc.Driver";
+		String username = "mdpAdmin";
+		String password = "mdppaketshop";
+		try {
+
+			Class.forName(dbClass);
+			Connection connection = DriverManager.getConnection(dbUrl,
+					username, password);
+			connection.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
