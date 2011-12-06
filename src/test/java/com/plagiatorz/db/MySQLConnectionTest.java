caa9565ea411ce3441bf52b1d046069590dd68d2
Marius Mueller <mmuelle5@hsz-t.ch>
2011-12-06 10:19:03 +0100

    DB-Connectiontest funktionniert

--- src/test/java/com/plagiatorz/db/MySQLConnectionTest.java
+++ src/test/java/com/plagiatorz/db/MySQLConnectionTest.java
@@ -2,7 +2,9 @@ package com.plagiatorz.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import junit.framework.TestCase;
 
@@ -10,24 +12,38 @@ import junit.framework.TestCase;
 public class MySQLConnectionTest extends TestCase {
 
 	public void testConnection() {
-		String dbUrl = "jdbc:mysql://www.fun-diver.ch:3306/paketshop";
+		String dbUrl = "jdbc:mysql://www.fun-diver.ch/paketshop";
 		String dbClass = "com.mysql.jdbc.Driver";
 		String username = "mdpAdmin";
 		String password = "mdppaketshop";
+		String query = "SELECT 1 FROM dual";
+		Connection connection = null;
 		try {
-  
-			Class.forName(dbClass);
-			Connection connection = DriverManager.getConnection(dbUrl,
-					username, password);
-			connection.close();
 
-	        assertTrue( true );
+			new com.mysql.jdbc.Driver();
+			Class.forName(dbClass);
+			connection = DriverManager.getConnection(dbUrl,
+					username, password);
+			
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			
+			resultSet.next();
+
+	        assertTrue( resultSet.getLong(1) == 1);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 	        assertTrue( false );
 		} catch (SQLException e) {
 			e.printStackTrace();
 	        assertTrue( false );
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+		        assertTrue( false );
+			}
 		}
 	}
 }
