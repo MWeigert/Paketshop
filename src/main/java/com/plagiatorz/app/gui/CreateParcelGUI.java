b0c17b21fd680b36f67b28f058c9135acad248d4
MWeigert <MWeigert@zhaw.ch>
2012-01-10 23:00:56 +0100

    Paket Erfassen GUI angelegt

---
+++ src/main/java/com/plagiatorz/app/gui/CreateParcelGUI.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.plagiatorz.app.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.plagiatorz.app.actions.actionListener.LoginAction;
+import com.plagiatorz.app.bean.LoginBean;
+import com.plagiatorz.app.bean.PaketBean;
+
+/**
+ * @author M. Weigert
+ *
+ */
+public class CreateParcelGUI implements BaseGUIInterface {
+
+	/**
+	 * 
+	 */
+	public CreateParcelGUI(JFrame jFrame, JPanel jPanel) {
+		super();
+		paketBean = new PaketBean(jFrame, jPanel);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.plagiatorz.app.gui.BaseGUIInterface#displayContentPanel()
+	 */
+	@Override
+	public void displayContentPanel() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
